d4f6edd4623e63e3fe34e8bebb5e16ef6f2a4e16
Jamie Hanlon <jamieh@graphcore.ai>
2020-12-17 12:34:39 +0000

    Convert test utility functions to macros for better reporting

--- tests/PathTests.cpp
+++ tests/PathTests.cpp
@@ -7,19 +7,15 @@
 #include "tests/definitions.hpp"
 #include "TestContext.hpp"
 
-void checkVarReport(const netlist_paths::Vertex *vertex,
-                    const std::string astTypeStr,
-                    const std::string dtypeStr,
-                    const std::string name) {
-  BOOST_TEST(vertex->getAstTypeStr() == astTypeStr);
-  BOOST_TEST(vertex->getDTypeStr() == dtypeStr);
-  BOOST_TEST(vertex->getName() == name);
-}
+#define CHECK_VAR_REPORT(vertex, astTypeStr, dtypeStr, name) \
+  do { \
+    BOOST_TEST(vertex->getAstTypeStr() == astTypeStr); \
+    BOOST_TEST(vertex->getDTypeStr() == dtypeStr); \
+    BOOST_TEST(vertex->getName() == name); \
+  } while (0);
 
-void checkLogReport(const netlist_paths::Vertex *vertex,
-                    const std::string astTypeStr) {
-  BOOST_TEST(vertex->getAstTypeStr() == astTypeStr);
-}
+#define CHECK_LOG_REPORT(vertex, astTypeStr) \
+  BOOST_TEST(vertex->getAstTypeStr() == astTypeStr)
 
 BOOST_FIXTURE_TEST_CASE(path_exists, TestContext) {
   BOOST_CHECK_NO_THROW(compile("adder.sv"));
@@ -39,26 +35,26 @@ BOOST_FIXTURE_TEST_CASE(path_query_basic_assign_chain, TestContext) {
   BOOST_CHECK_NO_THROW(compile("basic_assign_chain.sv"));
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("in", "out"));
   BOOST_TEST(vertices.size() == 7);
-  checkVarReport(vertices[0], "VAR", "logic", "in");
-  checkLogReport(vertices[1], "ASSIGN");
-  checkVarReport(vertices[2], "VAR", "logic", "basic_assign_chain.a");
-  checkLogReport(vertices[3], "ASSIGN");
-  checkVarReport(vertices[4], "VAR", "logic", "basic_assign_chain.b");
-  checkLogReport(vertices[5], "ASSIGN");
-  checkVarReport(vertices[6], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(vertices[0], "VAR", "logic", "in");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[2], "VAR", "logic", "basic_assign_chain.a");
+  CHECK_LOG_REPORT(vertices[3], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[4], "VAR", "logic", "basic_assign_chain.b");
+  CHECK_LOG_REPORT(vertices[5], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[6], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_query_basic_comb_chain, TestContext) {
   BOOST_CHECK_NO_THROW(compile("basic_comb_chain.sv"));
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("in", "out"));
   BOOST_TEST(vertices.size() == 7);
-  checkVarReport(vertices[0], "VAR", "logic", "in");
-  checkLogReport(vertices[1], "ASSIGN");
-  checkVarReport(vertices[2], "VAR", "logic", "basic_comb_chain.a");
-  checkLogReport(vertices[3], "ASSIGN");
-  checkVarReport(vertices[4], "VAR", "logic", "basic_comb_chain.b");
-  checkLogReport(vertices[5], "ASSIGN");
-  checkVarReport(vertices[6], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(vertices[0], "VAR", "logic", "in");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[2], "VAR", "logic", "basic_comb_chain.a");
+  CHECK_LOG_REPORT(vertices[3], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[4], "VAR", "logic", "basic_comb_chain.b");
+  CHECK_LOG_REPORT(vertices[5], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[6], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_query_basic_ff_chain, TestContext) {
@@ -66,21 +62,21 @@ BOOST_FIXTURE_TEST_CASE(path_query_basic_ff_chain, TestContext) {
   // in -> a
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("in", "basic_ff_chain.a"));
   BOOST_TEST(vertices.size() == 3);
-  checkVarReport(vertices[0], "VAR", "logic", "in");
-  checkLogReport(vertices[1], "ASSIGN_DLY");
-  checkVarReport(vertices[2], "DST_REG", "logic", "basic_ff_chain.a");
+  CHECK_VAR_REPORT(vertices[0], "VAR", "logic", "in");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN_DLY");
+  CHECK_VAR_REPORT(vertices[2], "DST_REG", "logic", "basic_ff_chain.a");
   // a -> b
   vertices = np->getAnyPath(netlist_paths::Waypoints("basic_ff_chain.a", "basic_ff_chain.b"));
   BOOST_TEST(vertices.size() == 3);
-  checkVarReport(vertices[0], "SRC_REG", "logic", "basic_ff_chain.a");
-  checkLogReport(vertices[1], "ASSIGN_DLY");
-  checkVarReport(vertices[2], "DST_REG", "logic", "basic_ff_chain.b");
+  CHECK_VAR_REPORT(vertices[0], "SRC_REG", "logic", "basic_ff_chain.a");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN_DLY");
+  CHECK_VAR_REPORT(vertices[2], "DST_REG", "logic", "basic_ff_chain.b");
   // b -> out
   vertices = np->getAnyPath(netlist_paths::Waypoints("basic_ff_chain.b", "out"));
   BOOST_TEST(vertices.size() == 3);
-  checkVarReport(vertices[0], "SRC_REG", "logic", "basic_ff_chain.b");
-  checkLogReport(vertices[1], "ASSIGN");
-  checkVarReport(vertices[2], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(vertices[0], "SRC_REG", "logic", "basic_ff_chain.b");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[2], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_query_pipeline_module, TestContext) {
@@ -88,13 +84,13 @@ BOOST_FIXTURE_TEST_CASE(path_query_pipeline_module, TestContext) {
   // NOTE: can differentiate between the generate instances of the pipeline.
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("i_data", "data_q"));
   BOOST_TEST(vertices.size() == 7);
-  checkVarReport(vertices[0], "VAR", "[31:0] logic", "i_data");
-  checkLogReport(vertices[1], "ASSIGN");
-  checkVarReport(vertices[2], "VAR", "[31:0] logic [8:0]", "pipeline_module.routing");
-  checkLogReport(vertices[3], "ASSIGN");
-  checkVarReport(vertices[4], "VAR", "[31:0] logic", "pipeline_module.__Vcellinp__g_pipestage[0].u_pipestage__i_data");
-  checkLogReport(vertices[5], "ASSIGN_DLY");
-  checkVarReport(vertices[6], "DST_REG", "[31:0] logic", "pipeline_module.g_pipestage[0].u_pipestage.data_q");
+  CHECK_VAR_REPORT(vertices[0], "VAR", "[31:0] logic", "i_data");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[2], "VAR", "[31:0] logic [8:0]", "pipeline_module.routing");
+  CHECK_LOG_REPORT(vertices[3], "ASSIGN");
+  CHECK_VAR_REPORT(vertices[4], "VAR", "[31:0] logic", "pipeline_module.__Vcellinp__g_pipestage[0].u_pipestage__i_data");
+  CHECK_LOG_REPORT(vertices[5], "ASSIGN_DLY");
+  CHECK_VAR_REPORT(vertices[6], "DST_REG", "[31:0] logic", "pipeline_module.g_pipestage[0].u_pipestage.data_q");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_query_pipeline_loops, TestContext) {
@@ -102,9 +98,9 @@ BOOST_FIXTURE_TEST_CASE(path_query_pipeline_loops, TestContext) {
   // NOTE: cannot currently differentiate between elements of the data_q array.
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("i_data", "data_q"));
   BOOST_TEST(vertices.size() == 3);
-  checkVarReport(vertices[0], "VAR", "[31:0] logic", "i_data");
-  checkLogReport(vertices[1], "ASSIGN_DLY");
-  checkVarReport(vertices[2], "DST_REG", "[31:0] logic [7:0]", "pipeline_loops.data_q");
+  CHECK_VAR_REPORT(vertices[0], "VAR", "[31:0] logic", "i_data");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN_DLY");
+  CHECK_VAR_REPORT(vertices[2], "DST_REG", "[31:0] logic [7:0]", "pipeline_loops.data_q");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_query_pipeline_no_loops, TestContext) {
@@ -112,9 +108,9 @@ BOOST_FIXTURE_TEST_CASE(path_query_pipeline_no_loops, TestContext) {
   // NOTE: cannot currently differentiate between elements of the data_q array.
   auto vertices = np->getAnyPath(netlist_paths::Waypoints("data_q", "data_q"));
   BOOST_TEST(vertices.size() == 3);
-  checkVarReport(vertices[0], "SRC_REG", "[31:0] logic [2:0]", "pipeline_no_loops.data_q");
-  checkLogReport(vertices[1], "ASSIGN_DLY");
-  checkVarReport(vertices[2], "DST_REG", "[31:0] logic [2:0]", "pipeline_no_loops.data_q");
+  CHECK_VAR_REPORT(vertices[0], "SRC_REG", "[31:0] logic [2:0]", "pipeline_no_loops.data_q");
+  CHECK_LOG_REPORT(vertices[1], "ASSIGN_DLY");
+  CHECK_VAR_REPORT(vertices[2], "DST_REG", "[31:0] logic [2:0]", "pipeline_no_loops.data_q");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_all_paths, TestContext) {
@@ -122,17 +118,17 @@ BOOST_FIXTURE_TEST_CASE(path_all_paths, TestContext) {
   auto paths = np->getAllPaths(netlist_paths::Waypoints("in", "out"));
   BOOST_TEST(paths.size() == 3);
   BOOST_TEST(paths[0].size() == 5);
-  checkVarReport(paths[0][0], "VAR", "logic", "in");
-  checkVarReport(paths[0][2], "VAR", "logic", "multiple_paths.a");
-  checkVarReport(paths[0][4], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[0][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[0][2], "VAR", "logic", "multiple_paths.a");
+  CHECK_VAR_REPORT(paths[0][4], "VAR", "logic", "out");
   BOOST_TEST(paths[1].size() == 5);
-  checkVarReport(paths[1][0], "VAR", "logic", "in");
-  checkVarReport(paths[1][2], "VAR", "logic", "multiple_paths.b");
-  checkVarReport(paths[1][4], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[1][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[1][2], "VAR", "logic", "multiple_paths.b");
+  CHECK_VAR_REPORT(paths[1][4], "VAR", "logic", "out");
   BOOST_TEST(paths[2].size() == 5);
-  checkVarReport(paths[2][0], "VAR", "logic", "in");
-  checkVarReport(paths[2][2], "VAR", "logic", "multiple_paths.c");
-  checkVarReport(paths[2][4], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[2][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[2][2], "VAR", "logic", "multiple_paths.c");
+  CHECK_VAR_REPORT(paths[2][4], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_fan_out, TestContext) {
@@ -141,14 +137,14 @@ BOOST_FIXTURE_TEST_CASE(path_fan_out, TestContext) {
   auto paths = np->getAllFanOut("in");
   BOOST_TEST(paths.size() == 3);
   BOOST_TEST(paths[0].size() == 3);
-  checkVarReport(paths[0][0], "VAR", "logic", "in");
-  checkVarReport(paths[0][2], "DST_REG", "logic", "fan_out_in.a");
+  CHECK_VAR_REPORT(paths[0][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[0][2], "DST_REG", "logic", "fan_out_in.a");
   BOOST_TEST(paths[1].size() == 3);
-  checkVarReport(paths[1][0], "VAR", "logic", "in");
-  checkVarReport(paths[1][2], "DST_REG", "logic", "fan_out_in.b");
+  CHECK_VAR_REPORT(paths[1][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[1][2], "DST_REG", "logic", "fan_out_in.b");
   BOOST_TEST(paths[2].size() == 3);
-  checkVarReport(paths[2][0], "VAR", "logic", "in");
-  checkVarReport(paths[2][2], "DST_REG", "logic", "fan_out_in.c");
+  CHECK_VAR_REPORT(paths[2][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[2][2], "DST_REG", "logic", "fan_out_in.c");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_fan_in, TestContext) {
@@ -157,14 +153,14 @@ BOOST_FIXTURE_TEST_CASE(path_fan_in, TestContext) {
   auto paths = np->getAllFanIn("out");
   BOOST_TEST(paths.size() == 3);
   BOOST_TEST(paths[0].size() == 3);
-  checkVarReport(paths[0][0], "SRC_REG", "logic", "fan_out_in.a");
-  checkVarReport(paths[0][2], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[0][0], "SRC_REG", "logic", "fan_out_in.a");
+  CHECK_VAR_REPORT(paths[0][2], "VAR", "logic", "out");
   BOOST_TEST(paths[1].size() == 3);
-  checkVarReport(paths[1][0], "SRC_REG", "logic", "fan_out_in.b");
-  checkVarReport(paths[1][2], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[1][0], "SRC_REG", "logic", "fan_out_in.b");
+  CHECK_VAR_REPORT(paths[1][2], "VAR", "logic", "out");
   BOOST_TEST(paths[2].size() == 3);
-  checkVarReport(paths[2][0], "SRC_REG", "logic", "fan_out_in.c");
-  checkVarReport(paths[2][2], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[2][0], "SRC_REG", "logic", "fan_out_in.c");
+  CHECK_VAR_REPORT(paths[2][2], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_fan_out_modules, TestContext) {
@@ -173,14 +169,14 @@ BOOST_FIXTURE_TEST_CASE(path_fan_out_modules, TestContext) {
   auto paths = np->getAllFanOut("in");
   BOOST_TEST(paths.size() == 3);
   BOOST_TEST(paths[0].size() == 3);
-  checkVarReport(paths[0][0], "VAR", "logic", "in");
-  checkVarReport(paths[0][2], "DST_REG", "logic", "fan_out_in_modules.foo_a.x");
+  CHECK_VAR_REPORT(paths[0][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[0][2], "DST_REG", "logic", "fan_out_in_modules.foo_a.x");
   BOOST_TEST(paths[1].size() == 3);
-  checkVarReport(paths[1][0], "VAR", "logic", "in");
-  checkVarReport(paths[1][2], "DST_REG", "logic", "fan_out_in_modules.foo_b.x");
+  CHECK_VAR_REPORT(paths[1][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[1][2], "DST_REG", "logic", "fan_out_in_modules.foo_b.x");
   BOOST_TEST(paths[2].size() == 3);
-  checkVarReport(paths[2][0], "VAR", "logic", "in");
-  checkVarReport(paths[2][2], "DST_REG", "logic", "fan_out_in_modules.foo_c.x");
+  CHECK_VAR_REPORT(paths[2][0], "VAR", "logic", "in");
+  CHECK_VAR_REPORT(paths[2][2], "DST_REG", "logic", "fan_out_in_modules.foo_c.x");
 }
 
 BOOST_FIXTURE_TEST_CASE(path_fan_in_modules, TestContext) {
@@ -189,20 +185,20 @@ BOOST_FIXTURE_TEST_CASE(path_fan_in_modules, TestContext) {
   auto paths = np->getAllFanIn("out");
   BOOST_TEST(paths.size() == 3);
   BOOST_TEST(paths[0].size() == 6);
-  checkVarReport(paths[0][0], "SRC_REG", "logic", "fan_out_in_modules.foo_a.x");
-  checkVarReport(paths[0][2], "VAR", "logic", "fan_out_in_modules.a");
-  checkVarReport(paths[0][4], "VAR", "logic", "fan_out_in_modules.foo_a.out");
-  checkVarReport(paths[0][5], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[0][0], "SRC_REG", "logic", "fan_out_in_modules.foo_a.x");
+  CHECK_VAR_REPORT(paths[0][2], "VAR", "logic", "fan_out_in_modules.a");
+  CHECK_VAR_REPORT(paths[0][4], "VAR", "logic", "fan_out_in_modules.foo_a.out");
+  CHECK_VAR_REPORT(paths[0][5], "VAR", "logic", "out");
   BOOST_TEST(paths[1].size() == 6);\
-  checkVarReport(paths[1][0], "SRC_REG", "logic", "fan_out_in_modules.foo_b.x");
-  checkVarReport(paths[1][2], "VAR", "logic", "fan_out_in_modules.b");
-  checkVarReport(paths[1][4], "VAR", "logic", "fan_out_in_modules.foo_b.out");
-  checkVarReport(paths[1][5], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[1][0], "SRC_REG", "logic", "fan_out_in_modules.foo_b.x");
+  CHECK_VAR_REPORT(paths[1][2], "VAR", "logic", "fan_out_in_modules.b");
+  CHECK_VAR_REPORT(paths[1][4], "VAR", "logic", "fan_out_in_modules.foo_b.out");
+  CHECK_VAR_REPORT(paths[1][5], "VAR", "logic", "out");
   BOOST_TEST(paths[2].size() == 6);
-  checkVarReport(paths[2][0], "SRC_REG", "logic", "fan_out_in_modules.foo_c.x");
-  checkVarReport(paths[2][2], "VAR", "logic", "fan_out_in_modules.c");
-  checkVarReport(paths[2][4], "VAR", "logic", "fan_out_in_modules.foo_c.out");
-  checkVarReport(paths[2][5], "VAR", "logic", "out");
+  CHECK_VAR_REPORT(paths[2][0], "SRC_REG", "logic", "fan_out_in_modules.foo_c.x");
+  CHECK_VAR_REPORT(paths[2][2], "VAR", "logic", "fan_out_in_modules.c");
+  CHECK_VAR_REPORT(paths[2][4], "VAR", "logic", "fan_out_in_modules.foo_c.out");
+  CHECK_VAR_REPORT(paths[2][5], "VAR", "logic", "out");
 }
 
 BOOST_FIXTURE_TEST_CASE(one_avoid_point, TestContext) {
